e93d8cc759f41871c643168abc1a538225eb69a3
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-09-29 23:52:31 +0800

    MLB對戰

---
+++ hw-113-1/src/main/java/org/example/LoggingFile.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class LoggingFile {
+
+  // 一般都是用 class name 來作為 logger 的名字
+  public static final Logger logger = Logger.getLogger(LoggingFile.class.getName());
+
+}
---
+++ hw-113-1/src/main/java/org/example/Team.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Comparator;
+import java.util.Objects;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Team implements Comparator<Team> {
+  private String league;
+  private String teamName;
+  private int win;
+  private int lose;
+  private float pct;
+
+  @Override
+  public int compare(Team team1, Team team2) {
+    if (team1.win < team2.win) {  // 比勝場數量
+      return 1;
+
+    } else if (team1.win > team2.win) {
+      return -1;
+
+    } else {
+      if (team1.lose < team2.lose) {  // 比誰輸的少
+        return 1;
+      } else if (team1.lose > team2.lose) {
+        return -1;
+      }
+    }
+    return team1.teamName.compareTo(team2.teamName);
+  }
+}
