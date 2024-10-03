c405a8fb26071cddf523406a51d1a2b90f178bfa
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-10-03 13:53:33 +0800

    update regularSort_csv
    1. 修改沒有成功放入陣列的錯誤
    2. 修改讀檔的方式為, 指定路徑為data

--- hw-113-1/src/main/java/org/example/RegularSort_csv.java
+++ hw-113-1/src/main/java/org/example/RegularSort_csv.java
@@ -12,160 +12,164 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
 
-public class RegularSort {
+public class RegularSort_csv {
   // 一般都是用 class name 來作為 logger 的名字
-  private static final Logger logger = Logger.getLogger(RegularSort.class.getName());
+  private static final Logger logger = Logger.getLogger(RegularSort_csv.class.getName());
 
   public static void main(String[] args) {
 
     try {
       // 設定 FileHandler，將日誌寫入檔案 "app.log"
-      FileHandler fileHandler = new FileHandler("app.log", true); // true 表示追加到文件中
+      FileHandler fileHandler = new FileHandler("RegularSort_csv.log", true); // true 表示追加到文件中
       fileHandler.setFormatter(new SimpleFormatter()); // 設定格式為簡單格式
       logger.addHandler(fileHandler);
 //      logger.info("這是一條 INFO 等級的日誌訊息");
 
-      String fileName = "MLB_2024_regular.txt";  // 要打開的檔案名稱
+      String fileName = "MLB_2024_regular.csv";  // 要打開的檔案名稱
+      String filePath = "data/" + fileName;
+
       List<Team> _ALlist = new ArrayList<>();  // 建立陣列存放AL球隊
-      List<Team> _ALtop3list = new ArrayList<>();  // 建立陣列存放AL top3球隊
+      List<Team> _ALtop3List = new ArrayList<>();  // 建立陣列存放AL top3球隊
       List<Team> _NLlist = new ArrayList<>();  // 建立陣列存放NL球隊
-      List<Team> _NLtop3list = new ArrayList<>();  // 建立陣列存放AL top3球隊
-      List<Team> team_ALEastlist = new ArrayList<>();  // 建立陣列存放AL East球隊
+      List<Team> _NLtop3List = new ArrayList<>();  // 建立陣列存放AL top3球隊
+      List<Team> team_ALEastList = new ArrayList<>();  // 建立陣列存放AL East球隊
       List<Team> team_ALCentralList = new ArrayList<>();  // 建立陣列存放AL Center球隊
-      List<Team> team_ALWestlist = new ArrayList<>();  // 建立陣列存放AL West球隊
-      List<Team> team_NLEastlist = new ArrayList<>();  // 建立陣列存放NL East球隊
+      List<Team> team_ALWestList = new ArrayList<>();  // 建立陣列存放AL West球隊
+      List<Team> team_NLEastList = new ArrayList<>();  // 建立陣列存放NL East球隊
       List<Team> team_NLCentralList = new ArrayList<>();  // 建立陣列存放NL Center球隊
-      List<Team> team_NLWestlist = new ArrayList<>();  // 建立陣列存放NL West球隊
+      List<Team> team_NLWestList = new ArrayList<>();  // 建立陣列存放NL West球隊
 
       // 開檔案
-      try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
-        logger.info("嘗試開啟檔案: " + fileName);
+      try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+        logger.info("嘗試開啟檔案: " + filePath);
 
-        String line = br.readLine();
-        while (line != null) {  // 讀每一row直到沒資料
-          assert line != null: "讀不到資料";
+        int count = 0;
+        String line = "";
+        while ((line = br.readLine()) != null) {  // 讀每一row直到沒資料
+          assert line != null : "讀不到資料";
+
+          count++;
+          if (count == 1) {  // 用來跳過第一行的表頭
+            continue;
+          }
+
+          line = line.replace("\"", "");  // 去除csv檔, 每行資料前後的""
           String[] data = line.split(",");
 
           if (data.length < 4) {  // 跳過不足4個欄位的資料
             continue;
           }
-          assert data.length >= 4: "欄位數不足4欄，資料格式可能有誤";
+          assert data.length >= 4 : "欄位數不足4欄，資料有誤";
 
           /*
-          用if/else區分League:
-          AL EAST, AL CENTER, AL WEST.
-          NL EAST, NL CENTER, NL WEST.
+          1. 用if/else區分League:
+             AL EAST, AL CENTER, AL WEST
+             NL EAST, NL CENTER, NL WEST
+          2. 分別放入該區域的陣列
            */
           if (data[0].equals("AL EAST")) { // AL EAST
-            assert data[0].equals("AL EAST"): "非 AL EAST 區";
-
-            processTeams(br, team_ALEastlist, "AL EAST");
-            line = br.readLine(); // 跳到下一行
+            assert data[0].equals("AL EAST") : "非 AL EAST 區";
+            processTeams(br, team_ALEastList, "AL EAST");
 
           } else if (data[0].equals("AL CENTRAL")) { // AL CENTRAL
-            assert data[0].equals("AL CENTRAL"): "非 AL CENTRAL 區";
-
+            assert data[0].equals("AL CENTRAL") : "非 AL CENTRAL 區";
             processTeams(br, team_ALCentralList, "AL CENTRAL");
-            line = br.readLine(); // 跳到下一行
 
           } else if (data[0].equals("AL WEST")) { // AL WEST
-            assert data[0].equals("AL WEST"): "非 AL WEST 區";
-
-            processTeams(br, team_ALWestlist, "AL WEST");
-            line = br.readLine(); // 跳到下一行
+            assert data[0].equals("AL WEST") : "非 AL WEST 區";
+            processTeams(br, team_ALWestList, "AL WEST");
 
           } else if (data[0].equals("NL EAST")) { // NL EAST
-            assert data[0].equals("NL EAST"): "非 NL EAST 區";
-
-            processTeams(br, team_NLEastlist, "NL EAST");
-            line = br.readLine(); // 跳到下一行
+            assert data[0].equals("NL EAST") : "非 NL EAST 區";
+            processTeams(br, team_NLEastList, "NL EAST");
 
           } else if (data[0].equals("NL CENTRAL")) { // NL CENTRAL
-            assert data[0].equals("NL CENTRAL"): "非 NL CENTRAL 區";
+            assert data[0].equals("NL CENTRAL") : "非 NL CENTRAL 區";
+            processTeams(br, team_NLCentralList, "NL CENTRAL");
 
-            processTeams(br, team_NLCentralList, "NL CENTER");
-            line = br.readLine(); // 跳到下一行
+          } else if (data[0].equals("NL WEST")) { // NL WEST
+            assert data[0].equals("NL WEST") : "非 NL WEST 區";
+            processTeams(br, team_NLWestList, "NL WEST");
 
-          } else if (data[0].equals("NL WEST")){ // NL WEST
-            assert data[0].equals("NL WEST"): "非 NL WEST 區";
-
-            processTeams(br, team_NLWestlist, "NL WEST");
-            line = br.readLine(); // 跳到下一行
           } // end of if/else
         } // end of while()
 
         System.out.println("AL EAST 排序前: ");
-        for (Team team: team_ALEastlist) {
+        for (Team team : team_ALEastList) {
           System.out.println(team);
         }
 
         System.out.println("AL CENTRAL 排序前: ");
-        for (Team team: team_ALCentralList) {
+        for (Team team : team_ALCentralList) {
           System.out.println(team);
         }
 
         System.out.println("AL WEST 排序前: ");
-        for (Team team: team_ALWestlist) {
+        for (Team team : team_ALWestList) {
           System.out.println(team);
         }
 
         System.out.println("NL EAST 排序前: ");
-        for (Team team: team_NLEastlist) {
+        for (Team team : team_NLEastList) {
           System.out.println(team);
         }
 
         System.out.println("NL CENTRAL 排序前: ");
-        for (Team team: team_NLCentralList) {
+        for (Team team : team_NLCentralList) {
           System.out.println(team);
         }
 
         System.out.println("NL WEST 排序前: ");
-        for (Team team: team_NLWestlist) {
+        for (Team team : team_NLWestList) {
           System.out.println(team);
         }
 
         // 6區各隊伍做排序
         //排序 (win多至少)
-        Collections.sort(team_ALEastlist, new Team());
+        Collections.sort(team_ALEastList, new Team());
         Collections.sort(team_ALCentralList, new Team());
-        Collections.sort(team_ALWestlist, new Team());
-        Collections.sort(team_NLEastlist, new Team());
+        Collections.sort(team_ALWestList, new Team());
+        Collections.sort(team_NLEastList, new Team());
         Collections.sort(team_NLCentralList, new Team());
-        Collections.sort(team_NLWestlist, new Team());
+        Collections.sort(team_NLWestList, new Team());
 
 
         System.out.println("排序後: ");
         for (int i = 0; i < 5; i++) {
-          System.out.println(team_ALEastlist.get(i));
+          System.out.println(team_ALEastList.get(i));
         }
 
         /*
         AL/NL各區冠軍分別進入該區top3陣列, 其餘隊伍分別進入AL/NL陣列
          */
-        divideTop3(_ALtop3list, _ALlist, team_ALEastlist);
-        divideTop3(_ALtop3list, _ALlist, team_ALCentralList);
-        divideTop3(_ALtop3list, _ALlist, team_ALWestlist);
-        divideTop3(_NLtop3list, _NLlist, team_NLEastlist);
-        divideTop3(_NLtop3list, _NLlist, team_NLCentralList);
-        divideTop3(_NLtop3list, _NLlist, team_NLWestlist);
+        divideTop3(_ALtop3List, _ALlist, team_ALEastList);
+        divideTop3(_ALtop3List, _ALlist, team_ALCentralList);
+        divideTop3(_ALtop3List, _ALlist, team_ALWestList);
+        divideTop3(_NLtop3List, _NLlist, team_NLEastList);
+        divideTop3(_NLtop3List, _NLlist, team_NLCentralList);
+        divideTop3(_NLtop3List, _NLlist, team_NLWestList);
 
         // 分別把前3名隊伍做排序、第4~12名隊伍做排序
         // 排序 (win由多而少)
-        Collections.sort(_ALtop3list, new Team());
+        Collections.sort(_ALtop3List, new Team());
         Collections.sort(_ALlist, new Team());
-        Collections.sort(_NLtop3list, new Team());
+        Collections.sort(_NLtop3List, new Team());
         Collections.sort(_NLlist, new Team());
 
         // 取得AL聯盟1~3名
-        Team ALp1 = null; Team ALp2 = null; Team ALp3 = null;
+        Team ALp1 = null;
+        Team ALp2 = null;
+        Team ALp3 = null;
         for (int i = 0; i < 1; i++) {
-          ALp1 = _ALtop3list.get(0);
-          ALp2 = _ALtop3list.get(1);
-          ALp3 = _ALtop3list.get(2);
+          ALp1 = _ALtop3List.get(0);
+          ALp2 = _ALtop3List.get(1);
+          ALp3 = _ALtop3List.get(2);
         }
 
         // 取得AL聯盟4~6名
-        Team ALp4 = null; Team ALp5 = null; Team ALp6 = null;
+        Team ALp4 = null;
+        Team ALp5 = null;
+        Team ALp6 = null;
         for (int i = 0; i < 1; i++) {
           ALp4 = _ALlist.get(0);
           ALp5 = _ALlist.get(1);
@@ -173,15 +177,19 @@ public class RegularSort {
         }
 
         // 取得NL聯盟1~3名
-        Team NLp1 = null; Team NLp2 = null; Team NLp3 = null;
+        Team NLp1 = null;
+        Team NLp2 = null;
+        Team NLp3 = null;
         for (int i = 0; i < 1; i++) {
-          NLp1 = _NLtop3list.get(0);
-          NLp2 = _NLtop3list.get(1);
-          NLp3 = _NLtop3list.get(2);
+          NLp1 = _NLtop3List.get(0);
+          NLp2 = _NLtop3List.get(1);
+          NLp3 = _NLtop3List.get(2);
         }
 
         // 取得NL聯盟4~6名
-        Team NLp4 = null; Team NLp5 = null; Team NLp6 = null;
+        Team NLp4 = null;
+        Team NLp5 = null;
+        Team NLp6 = null;
         for (int i = 0; i < 1; i++) {
           NLp4 = _NLlist.get(0);
           NLp5 = _NLlist.get(1);
@@ -198,14 +206,14 @@ public class RegularSort {
         System.out.println();
         System.out.println("排程表: ");
         // 輸出格式
-        System.out.println("(AMERICAN LEAGUE)" );
+        System.out.println("(AMERICAN LEAGUE)");
         System.out.printf("%-21s 6 ----- \n", ALp6.getTeamName());
         System.out.printf("%-21s 3 ----- ? -----\n", ALp3.getTeamName());
         System.out.printf("%-29s 2 ----- ?\n", ALp2.getTeamName());
         System.out.printf("%-21s 5 -----\n", ALp5.getTeamName());
         System.out.printf("%-21s 4 ----- ? -----\n", ALp4.getTeamName());
         System.out.printf("%-29s 1 ----- ? ----- ?\n", ALp1.getTeamName());
-        System.out.printf("%-47s----- ?\n",' ');
+        System.out.printf("%-47s----- ?\n", ' ');
         System.out.printf("%-21s 6 ----- ? ----- ? ----- ?\n", NLp6.getTeamName());
         System.out.printf("%-21s 3 -----\n", NLp3.getTeamName());
         System.out.printf("%-29s 2 -----\n", NLp2.getTeamName());
@@ -236,18 +244,19 @@ public class RegularSort {
   private static void processTeams(BufferedReader br, List<Team> teamList, String league) throws IOException {
     for (int i = 0; i < 5; i++) {
       String line = br.readLine(); // 跳到標頭(例如:NL WEST)的下一行
+      line = line.replace("\"", "");  // 去除每行的前後""
       String[] data = line.split(",");
       Team team = new Team();
 
       try {
         String teamName = data[0];
-        assert data[1].matches("\\d+"): "讀入的W非數字";
+        assert data[1].matches("\\d+") : "讀入的W非數字";
         int win = Integer.parseInt(data[1]);
 
-        assert data[2].matches("\\d+"): "讀入的L非數字";
+        assert data[2].matches("\\d+") : "讀入的L非數字";
         int lose = Integer.parseInt(data[2]);
 
-        assert data[2].matches(".\\d+"): "讀入的PCT非浮點數";
+        assert data[2].matches(".\\d+") : "讀入的PCT非浮點數";
         float pct = Float.parseFloat(data[3]);
 
 
@@ -264,14 +273,14 @@ public class RegularSort {
         e.printStackTrace();
       }
     } // end of for()
-    assert teamList.size() == 5: "每個區應有5個隊伍";
+    assert teamList.size() == 5 : "每個區應有5個隊伍";
   } // end of processTeams
 
 
   private static void divideTop3(List<Team> top3List, List<Team> league, List<Team> teamList) {
     for (int i = 0; i < 1; i++) {
 //          System.out.println(teamALEastlist.get(i));
-      assert teamList.size() == 5: "每區應有5個隊伍";
+      assert teamList.size() == 5 : "每區應有5個隊伍";
       Team p1 = null;
       Team p2 = null;
       Team p3 = null;
