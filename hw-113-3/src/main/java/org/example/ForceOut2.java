8a885faf31a87aa6e537656b032ed2a379d5edbc
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-11-01 14:26:32 +0800

    Update ForceOut2.java

--- hw-113-3/src/main/java/org/example/ForceOut2.java
+++ hw-113-3/src/main/java/org/example/ForceOut2.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class ForceOut2 {
@@ -14,17 +15,24 @@ public class ForceOut2 {
      */
 
     int[] baseArray = new int[]{1, base1, base2, base3};  // 存放壘包人數
-    Set<Integer> forceOutSet = new HashSet<>();  // 存放壘包封殺狀況
+    Set<String> forceOutSet = new HashSet<>();  // 存放壘包封殺狀況
 
     for (int i = 1; i < baseArray.length; i++) {  // 從1壘開始查看
-      if (baseArray[i - 1] == 1) {
-        if (baseArray[i] == 1) {
-          forceOutSet.add(i);
-          forceOutSet.add(i + 1);
+      if (baseArray[i - 1] == 1) {  // 前一個壘包
+        if (baseArray[i] == 1) {  // 當下的壘包有人
+          forceOutSet.add(String.valueOf(i) + "B");
+
+          if ((i + 1) == 4) {
+            forceOutSet.add("HB");  // 本壘
+          } else {
+            forceOutSet.add(String.valueOf(i + 1) + "B");
+          }
+
+        } else {   // 當下的壘包無人
+          forceOutSet.add(String.valueOf(i) + "B");
         }
       }
     }
-
     return forceOutSet.toString();
   } // end of getForceOut
 
@@ -34,6 +42,7 @@ public class ForceOut2 {
     System.out.println(forceOut2.getForceOut(1,0,0));  // 1B, 2B
     System.out.println(forceOut2.getForceOut(1,0,1));  // 1B, 2B
     System.out.println(forceOut2.getForceOut(0,0,1));  // 1B
+    System.out.println(forceOut2.getForceOut(1,1,1));  // 1B, 2B, 3B, HB
 
   }
 
