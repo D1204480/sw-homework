6cacc8dd712975d0d157304d3be55bebff47804b
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-11-01 21:35:33 +0800

    update

--- hw-113-3/src/test/java/org/example/ForceOut2Test.java
+++ hw-113-3/src/test/java/org/example/ForceOut2Test.java
@@ -10,21 +10,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ForceOut2Test {
 
-    @Test
-    void getForceOut() {
+  @Test
+  void getForceOut() {
         /* 封殺狀況:
     x       -> 1B
     1B      -> 1B, 2B
     1B, 3B  -> 1B, 2B
     3B      -> 1B
      */
-        ForceOut2 forceOut2 = new ForceOut2();  // 建立物件
-        assertAll("test forceOut2 for getForceOut",
-                () -> assertEquals(Set.of("1B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(0, 0, 0).split(", ")))),
-                () -> assertEquals(Set.of("1B", "2B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 0, 0).split(", ")))),
-                () -> assertEquals(Set.of("1B", "2B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 0, 1).split(", ")))),
-                () -> assertEquals(Set.of("1B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(0, 0, 1).split(", ")))),
-                () -> assertEquals(Set.of("1B", "2B", "3B", "HB"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 1, 1).split(", "))))
-        );
-    }
+    ForceOut2 forceOut2 = new ForceOut2();  // 建立物件
+    assertAll("test forceOut2 for getForceOut",
+        () -> assertEquals(Set.of("1B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(0, 0, 0).split(", ")))),
+        () -> assertEquals(Set.of("1B", "2B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 0, 0).split(", ")))),
+        () -> assertEquals(Set.of("1B", "2B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 0, 1).split(", ")))),
+        () -> assertEquals(Set.of("1B"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(0, 0, 1).split(", ")))),
+        () -> assertEquals(Set.of("1B", "2B", "3B", "HB"), new HashSet<>(Arrays.asList(forceOut2.getForceOut(1, 1, 1).split(", "))))
+    );
+  }
 }
