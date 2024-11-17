21078e11c277b2e81d8df756f66948c78ba1207d
D1204480 <161423607+D1204480@users.noreply.github.com>
2024-11-17 11:03:53 +0800

    Create BatterTest.java

---
+++ hw-113-4/src/test/java/org/example/BatterTest.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class BatterTest {
+
+  @ParameterizedTest
+  @CsvFileSource(resources = "/base_hits_breakdown.csv", numLinesToSkip = 1)
+  void testWithCsvFileSource(int num, int num2) {
+    assertNotNull(num);
+    assertNotNull(num2);
+  }
+
+  @Test
+  void parseFile() {
+  }
+
+  @Test
+  void calHitsRate() {
+  }
+}
